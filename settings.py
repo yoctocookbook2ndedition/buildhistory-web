de095bee3a8fcee8cc56b11bd782186cf5a07a5a
Paul Eggleton <paul.eggleton@linux.intel.com>
2013-04-29 18:04:24 +0100

    settings.py: add clickjacking protection
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

--- settings.py
+++ settings.py
@@ -105,7 +105,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+# Clickjacking protection
+X_FRAME_OPTIONS = 'DENY'
 
 ROOT_URLCONF = 'urls'
 
