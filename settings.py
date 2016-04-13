de5a52247e992eff3435aa11e487de96475da787
Paul Eggleton <paul.eggleton@linux.intel.com>
2016-04-14 11:11:23 +1200

    Ensure brand link points to top level page
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 # Based on settings.py from the Django project template
 # Copyright (c) Django Software Foundation and individual contributors.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -14,8 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3 (full path recommended).
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/paul/poky/buildhistory-web/bhtest.db3',                      # Or path to database file if using sqlite3 (full path recommended).
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -90,7 +90,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = 'oiiwpoidpaowdawndmanw'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
