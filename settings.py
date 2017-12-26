07eaee5c920f3967833980c325b4c25617836090
Alex Gonzalez <alex@lindusembedded.com>
2017-12-26 14:59:22 +0100

    settings.py: Templates updated to Django 1.8
    See https://docs.djangoproject.com/en/1.10/ref/templates/upgrading/
    
    Signed-off-by: Alex Gonzalez <alex@lindusembedded.com>

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@
 # Copyright (c) Django Software Foundation and individual contributors.
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -15,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/paul/poky/buildhistory-web/bhtest.db3',                      # Or path to database file if using sqlite3 (full path recommended).
+        'NAME': '/opt/yocto/fsl-community-bsp/sources/buildhistory-web/bhtest.db3',                      # Or path to database file if using sqlite3 (full path recommended).
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -92,13 +91,6 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'oiiwpoidpaowdawndmanw'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -111,19 +103,28 @@ MIDDLEWARE_CLASSES = (
 # Clickjacking protection
 X_FRAME_OPTIONS = 'DENY'
 
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
-TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-    'django.core.context_processors.request',
-)
-
 ROOT_URLCONF = 'urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + "/templates",
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+#               'django.template.loaders.eggs.Loader',
+            ],
+            'debug': True,
+        },
+    },
+]
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -169,3 +170,4 @@ EMAIL_HOST = 'smtp.example.com'
 DEFAULT_FROM_EMAIL = 'noreply@example.com'
 LOGIN_REDIRECT_URL = '/warningmgr'
 
+ALLOWED_HOSTS = [u'aws-dey-builder']
